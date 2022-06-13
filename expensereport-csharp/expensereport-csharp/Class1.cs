222af58d303c4898986b9db57fe967e668d68882
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-06-13 11:33:32 +0500

    use inheritance

--- expensereport-csharp/expensereport-csharp/Class1.cs
+++ expensereport-csharp/expensereport-csharp/Class1.cs
@@ -8,10 +8,41 @@ namespace expensereport_csharp
         DINNER, BREAKFAST, CAR_RENTAL
     }
 
-    public class Expense
+    public abstract class Expense
     {
+        public abstract string name { get; }
         public ExpenseType type;
         public int amount;
+
+        public virtual string GetMarker()
+        {
+            return " ";
+        }
+    }
+
+    public class DinnerExpense : Expense
+    {
+        public override string name => "Dinner";
+
+        public override string GetMarker()
+        {
+            return amount > 5000 ? "X" : base.GetMarker();
+        }
+    }
+
+    public class BreakfastExpense : Expense
+    {
+        public override string name => "Breakfast";
+
+        public override string GetMarker()
+        {
+            return amount > 1000 ? "X" : base.GetMarker();
+        }
+    }
+
+    public class CarRentalExpense : Expense
+    {
+        public override string name => "CarRental";
     }
 
     public class ExpenseReport
@@ -22,10 +53,7 @@ namespace expensereport_csharp
 
             foreach (Expense expense in expenses)
             {
-                var expenseName = GetExpanseName(expense);
-                var expanseMarker = GetExpanseMarker(expense);
-
-                Console.WriteLine(expenseName + "\t" + expense.amount + "\t" + expanseMarker);
+                Console.WriteLine(expense.name + "\t" + expense.amount + "\t" + expense.GetMarker());
             }
 
             var mealExpenses = CalculateMealExpenses(expenses);
@@ -51,33 +79,13 @@ namespace expensereport_csharp
             int result = 0;
             foreach (Expense expense in expenses)
             {
-                if (expense.type == ExpenseType.DINNER || expense.type == ExpenseType.BREAKFAST)
+                if (expense is DinnerExpense || expense is BreakfastExpense)
                 {
                     result += expense.amount;
                 }
             }
 
             return result;
-        }
-
-        private static string GetExpanseMarker(Expense expense)
-        {
-            return expense.type == ExpenseType.DINNER && expense.amount > 5000 || expense.type == ExpenseType.BREAKFAST && expense.amount > 1000 ? "X": " ";
-        }
-
-        private static string GetExpanseName(Expense expense)
-        {
-            switch (expense.type)
-            {
-                case ExpenseType.DINNER:
-                    return "Dinner";
-                case ExpenseType.BREAKFAST:
-                    return "Breakfast";
-                case ExpenseType.CAR_RENTAL:
-                    return "Car Rental";
-                default:
-                    return string.Empty;
-            }
         }
     }
 }
