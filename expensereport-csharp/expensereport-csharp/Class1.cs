ecd4d8ec613ae3532339b94fbf1dec89d00fffcc
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-06-13 12:04:39 +0500

    rename variable

--- expensereport-csharp/expensereport-csharp/Class1.cs
+++ expensereport-csharp/expensereport-csharp/Class1.cs
@@ -6,17 +6,17 @@ namespace expensereport_csharp
 {
     public class Expense
     {
-        public string name { get; }
+        public string Name { get; }
         public bool IsMeal { get; }
         public bool IsOverLimit { get; }
-        public int amount;
+        public int Amount { get; }
         private readonly int limit;
 
         public Expense(string name, bool isMeal = false, int limit = int.MaxValue)
         {
             this.limit = limit;
             this.IsMeal = isMeal;
-            this.name = name;
+            this.Name = name;
         }
 
         public static Expense Dinner => new("Dinner", true, 5000);
@@ -49,7 +49,7 @@ namespace expensereport_csharp
 
         private static string GenerateRow(Expense expense)
         {
-            return expense.name + "\t" + expense.amount + "\t" + GetMarker(expense);
+            return expense.Name + "\t" + expense.Amount + "\t" + GetMarker(expense);
         }
 
         private static string GetMarker(Expense expense)
@@ -65,12 +65,12 @@ namespace expensereport_csharp
 
         private static int CalculateMealExpenses(List<Expense> expenses)
         {
-            return expenses.Where(x => x.IsMeal).Sum(x => x.amount);
+            return expenses.Where(x => x.IsMeal).Sum(x => x.Amount);
         }
 
         private static int CalculateTotal(List<Expense> expenses)
         {
-            return expenses.Sum(x => x.amount);
+            return expenses.Sum(x => x.Amount);
         }
     }
 }
